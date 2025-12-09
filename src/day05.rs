774c438006a68e28570ebc71824911df0e1ea229
ThiFerNe <thimo.f.neumann@gmail.com>
2025-12-09 13:36:09 +0100

    Add Day 5 Part 1

---
+++ src/day05.rs
@@ -0,0 +1,166 @@
+use std::num::ParseIntError;
+use std::str::FromStr;
+
+fn main() {
+    aoc_2025::aoc!(INPUT, part1);
+}
+
+const INPUT: &str = include_str!("../input/input.day05");
+
+#[cfg(feature = "part1")]
+fn part1(input: &str) -> u64 {
+    // Took 16 minutes 30,47 seconds
+    Database::from_str(input)
+        .expect("Should parse fine")
+        .count_fresh_available_ingredients()
+}
+
+#[derive(Clone, Eq, PartialEq, Hash, Debug)]
+struct Database {
+    fresh_ingredient_ranges: Box<[IngredientIdRange]>,
+    available_ingredients: Box<[IngredientId]>,
+}
+
+impl Database {
+    fn count_fresh_available_ingredients(&self) -> u64 {
+        self.available_ingredients
+            .iter()
+            .filter(|available_ingredient| {
+                self.fresh_ingredient_ranges
+                    .iter()
+                    .any(|fresh_ingredient_range| {
+                        fresh_ingredient_range.contains(available_ingredient)
+                    })
+            })
+            .count() as u64
+    }
+}
+
+impl FromStr for Database {
+    type Err = ParseDatabaseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut fresh_ingredient_ranges = Vec::new();
+        let mut available_ingredients = Vec::new();
+        let mut separator_line_found = false;
+        for (index, line) in s.lines().enumerate() {
+            if line.is_empty() {
+                separator_line_found = true;
+            } else if separator_line_found {
+                available_ingredients.push(line.parse().map_err(|error| {
+                    ParseDatabaseError::ParseAvailableIngredientId {
+                        index,
+                        source: error,
+                    }
+                })?);
+            } else {
+                fresh_ingredient_ranges.push(line.parse().map_err(|error| {
+                    ParseDatabaseError::ParseFreshIngredientRange {
+                        index,
+                        source: error,
+                    }
+                })?);
+            }
+        }
+        Ok(Self {
+            fresh_ingredient_ranges: fresh_ingredient_ranges.into_boxed_slice(),
+            available_ingredients: available_ingredients.into_boxed_slice(),
+        })
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+enum ParseDatabaseError {
+    #[error("Failed parsing line {index} as fresh ingredient range: {source}")]
+    ParseFreshIngredientRange {
+        index: usize,
+        source: ParseIngredientRangeError,
+    },
+    #[error("Failed parsing line {index} as available ingredient id: {source}")]
+    ParseAvailableIngredientId {
+        index: usize,
+        source: ParseIngredientIdError,
+    },
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
+struct IngredientIdRange {
+    from: IngredientId,
+    inclusive_to: IngredientId,
+}
+
+impl IngredientIdRange {
+    fn contains(&self, id: &IngredientId) -> bool {
+        self.from <= *id && *id <= self.inclusive_to
+    }
+}
+
+impl FromStr for IngredientIdRange {
+    type Err = ParseIngredientRangeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (from, to) = s
+            .split_once('-')
+            .ok_or(ParseIngredientRangeError::MissingDelimiter)?;
+        Ok(Self {
+            from: from.parse().map_err(ParseIngredientRangeError::ParseFrom)?,
+            inclusive_to: to
+                .parse()
+                .map_err(ParseIngredientRangeError::ParseInclusiveTo)?,
+        })
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+enum ParseIngredientRangeError {
+    #[error("Missing delimiter")]
+    MissingDelimiter,
+    #[error("Failed to parse from: {0}")]
+    ParseFrom(#[source] ParseIngredientIdError),
+    #[error("Failed to parse inclusive to: {0}")]
+    ParseInclusiveTo(#[source] ParseIngredientIdError),
+}
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+struct IngredientId(u64);
+
+impl FromStr for IngredientId {
+    type Err = ParseIngredientIdError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(s.parse()?))
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+enum ParseIngredientIdError {
+    #[error("Failed to parse id as number: {0}")]
+    Parse(#[from] ParseIntError),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        // Arrange
+        let input = "3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32";
+
+        // Act
+        let part1 = part1(input);
+
+        // Assert
+        assert_eq!(part1, 3);
+    }
+}
